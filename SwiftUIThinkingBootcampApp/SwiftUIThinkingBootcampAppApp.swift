0b82349e08187a3fed71516b2b1183884c77ec97
Valerii <v.du@dcjudgement.com>
2023-01-22 15:52:29 +0200

    Initial Commit

---
+++ SwiftUIThinkingBootcampApp/SwiftUIThinkingBootcampAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampAppApp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by User03 on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

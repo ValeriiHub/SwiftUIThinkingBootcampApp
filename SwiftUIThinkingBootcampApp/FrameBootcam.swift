8683f6e5dc84a429285083ec01a82aed3231951b
Valerii <v.du@dcjudgement.com>
2023-01-25 23:54:42 +0200

    Added Frame

---
+++ SwiftUIThinkingBootcampApp/FrameBootcam.swift
@@ -0,0 +1,24 @@
+//
+//  FrameBootcamp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by User03 on 25.01.2023.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+//            .frame(width: 100, height: 100, alignment: .center)
+//            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+//            .background(Color.blue)
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}

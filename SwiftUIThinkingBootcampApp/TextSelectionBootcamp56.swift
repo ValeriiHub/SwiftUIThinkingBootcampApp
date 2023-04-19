b462b760b71a92626496fb737d20d38e17d9d2c8
Valerii <v.du@dcjudgement.com>
2023-04-19 09:00:06 +0300

    Added TextSelection

---
+++ SwiftUIThinkingBootcampApp/TextSelectionBootcamp56.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp56.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by User03 on 19.04.2023.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp56: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp56_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp56()
+    }
+}

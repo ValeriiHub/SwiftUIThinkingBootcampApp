95c255fbe2fe81860a87fa26311ff57ec90a9bc5
Valerii <v.du@dcjudgement.com>
2023-04-19 09:21:21 +0300

    Added ButtonStyle

---
+++ SwiftUIThinkingBootcampApp/ButtonStyleBootcamp57.swift
@@ -0,0 +1,94 @@
+//
+//  ButtonStyleBootcamp57.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by User03 on 19.04.2023.
+//
+
+import SwiftUI
+
+struct ButtonStyleBootcamp57: View {
+    var body: some View {
+        VStack {
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.plain)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.bordered)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderless)
+            
+            // -----
+            Divider()
+            
+            Button {
+                
+            } label: {
+                Text("Button Title")
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .controlSize(.large)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.regular)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.small)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.mini)
+        }
+    }
+}
+
+struct ButtonStyleBootcamp57_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStyleBootcamp57()
+    }
+}
